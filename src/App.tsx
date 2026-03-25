a57a48bf07072b95a2e8518ba7a05c31c055f1db
Pedrohenriquezuza <phzuza07@gmail.com>
2026-03-25 20:17:42 -0300

    Created DialogComponent using Radix-ui

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,14 @@ import InputCheckBox from "./components/Input-CheckBox";
 import InputSingleFile from "./components/Input-Single-File";
 import { useForm } from "react-hook-form";
 import ImageFilePreview from "./components/image-file-preview";
+import { Dialog, DialogClose, DialogTrigger } from "@radix-ui/react-dialog";
+import {
+  DialogBody,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+} from "./components/dialog";
+import Text from "./components/text";
 
 export default function App() {
   const form = useForm();
@@ -76,6 +84,40 @@ export default function App() {
           replaceBy={<ImageFilePreview src={fileSource} alt="Imagem" />}
           {...form.register("file")}
         />
+      </div>
+
+      <div>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button>Abrir Modal</Button>
+          </DialogTrigger>
+
+          <DialogContent>
+            <DialogHeader>Teste Dialogue</DialogHeader>
+
+            <DialogBody>
+              <Text as="div" className="mb-4">
+                TESTE CONTEÚDO DO DIALOG
+              </Text>
+              <InputSingleFile
+                form={form}
+                allowedExtensions={["png", "jpg", "jpeg", "webp"]}
+                maxFileSizeInMB={50}
+                replaceBy={<ImageFilePreview src={fileSource} alt="Imagem" />}
+                {...form.register("file")}
+              />
+            </DialogBody>
+
+            <DialogFooter>
+
+              <DialogClose asChild>
+                <Button variant="secondary">Cancelar</Button>
+              </DialogClose>
+
+                <Button>Adicionar</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </div>
     </div>
   );
