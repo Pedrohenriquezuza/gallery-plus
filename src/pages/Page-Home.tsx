e54eef4639e9ba3634c308c9d3340671149402b3
Pedrohenriquezuza <phzuza07@gmail.com>
2026-04-09 18:24:42 -0300

    Mock do filtro de álbuns

--- src/pages/Page-Home.tsx
+++ src/pages/Page-Home.tsx
@@ -1,9 +1,19 @@
 import Container from "../components/container";
+import AlbumsFilter from "../contexts/albums/components/albums-filter";
 import PhotosList from "../contexts/photos/components/photos-list";
 
 export default function Home() {
   return (
     <Container>
+      <AlbumsFilter
+        albums={[
+          { id: "1234", title: "Album 1" },
+          { id: "4321", title: "Album 2" },
+          { id: "8976", title: "Album 3" },
+        ]}
+        loading
+        className="mb-9"
+      />
       <PhotosList photos={[]} loading />
     </Container>
   );
